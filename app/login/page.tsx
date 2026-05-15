e4203b8c9ad4a3ac63d8ce333d779aa6a9e42f27
SyedHassaanRaheel <hassan.raheel@enviovista.com>
2026-05-15 05:09:17 +0500

    My latest changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -107,7 +107,7 @@ export default function LoginPage() {
           
           <div className="text-center">
             <a href="/register" className="text-sm text-indigo-600 hover:text-indigo-500">
-              Don't have an account? Register why are you here 
+              Don't have an account? Register why are you here now
             </a>
           </div>
         </form>
